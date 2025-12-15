06edbdccc9fd759c0734021e2e47d33c7c3c28db
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-12-15 14:23:04 +0200

    Create RefreshToken.cs

---
+++ api/CantinaManager/CantinaManager/Auth/Models/RefreshToken.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CantinaManager.Models
+{
+    public class RefreshToken
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Token { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+        public DateTime Expires { get; set; }
+        public bool IsRevoked { get; set; } = false;
+
+        [ForeignKey("UserId")]
+        public User User { get; set; } = null!;
+    }
+}
