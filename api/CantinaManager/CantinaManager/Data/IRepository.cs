129b04655472e79a0c0f326730e98be846179af8
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-12-15 11:34:22 +0200

    Repository interface

---
+++ api/CantinaManager/CantinaManager/Data/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using CantinaManager.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CantinaManager.Data
+{
+    public interface IRepository
+    {
+        Task<User> CreateUserAsync(User user);
+        Task<User?> GetUserByIdAsync(string userId);
+        Task<User?> GetUserByUsernameAsync(string username);
+        Task<List<User>> GetAllUsersAsync();
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(string userId);
+
+        Task<UserTask> CreateTaskAsync(UserTask task);
+        Task<UserTask?> GetTaskByIdAsync(int taskId);
+
+        Task<List<UserTask>> GetTasksByUserIdAsync(string userId, int offset = 0, int limit = 10);
+
+        Task UpdateTaskAsync(UserTask task);
+        Task DeleteTaskAsync(int taskId);
+    }
+}
