a83a23ee295cdcfe993044d14a9d764cfeeb8266
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-12-18 08:14:31 +0200

    Update JwtService.cs

--- api/CantinaManager/CantinaManager/Auth/JwtService.cs
+++ api/CantinaManager/CantinaManager/Auth/JwtService.cs
@@ -1,45 +1,57 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using CantinaManager.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using CantinaManager.Models;
+using System.Threading.Tasks;
 
-public class JwtService
+namespace CantinaManager.Services
 {
-    private readonly IConfiguration _config;
-
-    public JwtService(IConfiguration config)
+    public class JwtService
     {
-        _config = config;
-    }
+        private readonly IConfiguration _config;
+        private readonly UserManager<User> _userManager;
 
-    public string GenerateAccessToken(User user)
-    {
-        var claims = new List<Claim>
+        public JwtService(IConfiguration config, UserManager<User> userManager)
         {
-            new Claim(JwtRegisteredClaimNames.Sub, user.Id),
-            new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName),
-            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-        };
+            _config = config;
+            _userManager = userManager;
+        }
 
-        claims.AddRange(user.UserRoles.Select(r => new Claim(ClaimTypes.Role, r.RoleName)));
+        public async Task<string> GenerateAccessTokenAsync(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
 
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var roles = await _userManager.GetRolesAsync(user);
+            claims.AddRange(roles.Select(r => new Claim(ClaimTypes.Role, r)));
 
-        var token = new JwtSecurityToken(
-            issuer: _config["Jwt:Issuer"],
-            audience: _config["Jwt:Audience"],
-            claims: claims,
-            expires: DateTime.UtcNow.AddMinutes(int.Parse(_config["Jwt:ExpireMinutes"])),
-            signingCredentials: creds
-        );
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-        return new JwtSecurityTokenHandler().WriteToken(token);
-    }
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(int.Parse(_config["Jwt:ExpireMinutes"])),
+                signingCredentials: creds
+            );
 
-    public string GenerateRefreshToken()
-    {
-        return Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public string GenerateRefreshToken()
+        {
+            return Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+        }
     }
 }
