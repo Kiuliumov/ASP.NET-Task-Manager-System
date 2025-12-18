3bf10bdb65e24308379b359b81082f6d09119e27
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-12-18 14:15:30 +0200

    Create EmailService.cs

---
+++ api/CantinaManager/CantinaManager/Services/EmailService/EmailService.cs
@@ -0,0 +1,38 @@
+﻿using CantinaManager.Services.EmailService;
+using System.Net;
+using System.Net.Mail;
+
+namespace CantinaManager.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string htmlMessage)
+        {
+            var smtpHost = _config["Email:SmtpHost"];
+            var smtpPort = int.Parse(_config["Email:SmtpPort"] ?? "587");
+            var smtpUser = _config["Email:SmtpUser"];
+            var smtpPass = _config["Email:SmtpPass"];
+            var fromEmail = _config["Email:From"];
+
+            using var client = new SmtpClient(smtpHost, smtpPort)
+            {
+                Credentials = new NetworkCredential(smtpUser, smtpPass),
+                EnableSsl = true
+            };
+
+            var mailMessage = new MailMessage(fromEmail, toEmail, subject, htmlMessage)
+            {
+                IsBodyHtml = true
+            };
+
+            await client.SendMailAsync(mailMessage);
+        }
+    }
+}
